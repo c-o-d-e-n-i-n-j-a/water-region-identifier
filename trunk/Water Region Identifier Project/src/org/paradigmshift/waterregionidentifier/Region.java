b21dff6b3452da70eb179826de393859e935e96d
cook.matthew.b@gmail.com <cook.matthew.b@gmail.com@d38da04e-e6a2-0839-c1d4-2287867de385>
2011-05-19 12:52:07 +0000

    Speed enhancements, multi thread safe enhancements, text formatting enhancements, and refactoring/renaming.

--- trunk/Water Region Identifier Project/src/org/paradigmshift/waterregionidentifier/Region.java
+++ trunk/Water Region Identifier Project/src/org/paradigmshift/waterregionidentifier/Region.java
@@ -1,42 +1,124 @@
 package org.paradigmshift.waterregionidentifier;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import org.apache.log4j.Logger;
+
 
 public final class Region {
 	
+	private static final Logger log = Logger.getLogger( Region.class );
+	
+	private static int maxLength = 0; 
+	
 	private long id;
-	private final Collection<WaterPixel> pixels = new HashSet<WaterPixel>();
+	private Collection<WaterPixel> pixels;
 	
 	public Region() {}
 	
-	public long getId() {
+	public static void identifyRegions( WaterMatrix matrix ) throws InterruptedException, ExecutionException, NumberFormatException, IOException {
+		
+		if ( log.isInfoEnabled() ) {
+			
+			log.info( "Processing the matrix..." );
+		}
+		
+		Map<Long, WaterPixelSorter> sorters = new HashMap<Long, WaterPixelSorter>();
+		Collection<Region> regions = new ArrayList<Region>();
+		
+		// Sort all the pixels by category - divide and conquer
+		for ( int y = 0; y < matrix.getNumRows(); y++ ) {
+			
+			for ( int x = 0; x < matrix.getNumCols(); x++ ) {
+				
+				WaterPixel pixel = matrix.getWaterPixel( x, y );
+				
+				// Add pixel to existing category sorter or create a new sorter
+				Long category = pixel.getCategory();
+				WaterPixelSorter sorter = sorters.get( category );
+				if ( sorter == null ) {
+					
+					sorter = new WaterPixelSorter( matrix, category );
+					sorters.put( category, sorter );
+				}
+				sorter.add( pixel );
+			}
+		}
+		
+		// Start sorting and wait for all tasks to complete
+		ExecutorService threadPool = Executors.newFixedThreadPool( 10 );
+		Collection<WaterPixelSorter> tasks = sorters.values();
+		for ( Future<Collection<Region>> f : threadPool.invokeAll( tasks ) ) {
+			
+			Collection<Region> collection = f.get();
+			if ( collection != null ) regions.addAll( collection );
+		}
+		threadPool.shutdown();
+		
+		// Set the ids on the regions
+		long regionId = 0;
+		for ( Region region : regions ) region.setId( regionId++ );
+		Region.maxLength = String.valueOf( regionId - 1 ).length();
+		if ( log.isInfoEnabled() ) log.info( "Processing complete: regions identified." );
+	}
+	
+	public static int getMaxLength() {
+		
+		return Region.maxLength;
+	}
+	
+	public synchronized long getId() {
 		
 		return this.id;
 	}
 	
-	public void setId( long id ) {
+	public synchronized void setId( long id ) {
 		
 		this.id = id;
 	}
 	
-	public void add( WaterPixel pixel ) {
+	public synchronized void add( WaterPixel pixel ) {
 		
+		if ( pixels == null ) pixels = new ArrayList<WaterPixel>();
 		pixels.add( pixel );
+		pixel.setRegion( this );
 	}
 	
-	public void addAll( Collection<WaterPixel> pixels ) {
+	public synchronized void merge( Region regionToMerge ) {
 		
-		this.pixels.addAll( pixels );
+		// Obtain a lock on the other region to prevent access to it till the pixels are updated
+		synchronized ( regionToMerge ) {
+			
+			Collection<WaterPixel> pixelsToMerge = regionToMerge.getPixels();
+			pixels.addAll( pixelsToMerge );
+			regionToMerge.removeAllPixels();
+			
+			for ( WaterPixel pixel : pixelsToMerge ) {
+				
+				pixel.setRegion( this );
+			}
+		}
 	}
 	
-	public Collection<WaterPixel> getPixels() {
+	public synchronized Collection<WaterPixel> getPixels() {
 		
 		return pixels;
 	}
 	
-	public boolean contains( WaterPixel pixel ) {
+	public synchronized boolean contains( WaterPixel pixel ) {
 		
 		return pixels.contains( pixel );
+	}
+	
+	public synchronized void removeAllPixels() {
+		
+		pixels = null;
 	}
 }
